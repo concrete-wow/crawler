909ee97d92388f3c003e65d9b2f7f5c7d5094b90
Rob Pickering <rob@pickering.org>
2020-04-26 23:06:30 +0100

    Reuse one crawler to reduce machine load once we have thousands of seed sites.

--- index.js
+++ index.js
@@ -16,69 +16,75 @@ const SOFTDELETE = `DELETE from url WHERE url = $1`
 const UPSERT_URL = `INSERT INTO url
                     (url, domain, crawler_rank, last_read) values ($1, $2, $3, $4)
                     ON CONFLICT (url)
-                    DO UPDATE SET crawler_rank = crawler_rank + $3, last_read=$4`;
+                    DO UPDATE SET crawler_rank = url.crawler_rank-($3), last_read=$4`;
 
 const UPSERT_LINK = `INSERT INTO link (src, dst, crawler_blessing, last_read) values ($1, $2, $3, $4)
                     ON CONFLICT (src, dst)
                     DO UPDATE SET crawler_blessing=$3, last_read=$4`;
 
 function crawl(url, rank) {
-  return new Promise((resolve, reject) => {
 
-    var crawler = new Crawler(url);
-    const now = new Date();
-
-    Object.assign(crawler, {
-      interval: 10000,
-      maxConcurrency: 10,
-      maxDepth: 100,
-      filterByDomain: false,
-      UserAgent
-    });
-    crawler.on("fetchcomplete", async function (item, data, res) {
-      if(item.url.match(config.ignoreFiles)){
-        console.log(`Ignoring file ${item.url}`);
-        return;
-      }
-      var finalise = this.wait();
-
-      try {
-        await db.none(UPSERT_URL,
-          [item.url, item.host, rank / item.depth, now]
-        );
-
-        if (item.referrer != null && !(new URL(item.referrer))
-          .pathname.match(/robots.txt/)) {
-          await db.none(UPSERT_URL,
-            [item.referrer, item.host, rank / item.depth, now]
-          );
-          await db.none(UPSERT_LINK,
-            [item.referrer, item.url, rank / item.depth, now]
-          );
-        } else {
-          if (item.depth > 1)
-            console.log('no referrer: ', item.url);
+  if (this.crawler == null) {
+    this.promise = new Promise((resolve, reject) => {
+      this.crawler = new Crawler(url);
+      Object.assign(this.crawler, {
+        interval: 1000,
+        maxConcurrency: 100,
+        maxDepth: 100,
+        filterByDomain: false,
+        UserAgent
+      });
+      this.crawler.on("fetchcomplete", async function (item, data, res) {
+        console.log('got url', item.url)
+        const now = new Date();
+        if (item.url.match(config.ignoreFiles)) {
+          console.log(`Ignoring file ${item.url}`);
+          return;
         }
-        // if we are at top level and only difference between thing we were called with and this
-        // is http://foo.bar.com vs http://foo.bar.com/ then kill the redundant one
-        // If we don't do this then the seed gets left behind with no fetch so we keep re-crawling it
-        // This is soft if there is a hanging referrer ref
-        if (item.depth === 1 && item.url !== url)
-          db.none(SOFTDELETE, [url])
-          .catch(() => {});
+        var finalise = this.wait();
 
-        console.log('inserted: ', item.url)
-      } catch (e) {
-        // We mostly aren't interested
-        console.log('insert failed: ', item.url)
-      } finally {
-        finalise();
-      }
+        try {
+          await db.none(UPSERT_URL,
+            [item.url, item.host, rank / item.depth, now]
+          );
+
+          if (item.referrer != null && !(new URL(item.referrer))
+            .pathname.match(/robots.txt/)) {
+            await db.none(UPSERT_URL,
+              [item.referrer, item.host, rank / item.depth, now]
+            );
+            await db.none(UPSERT_LINK,
+              [item.referrer, item.url, rank / item.depth, now]
+            );
+          } else {
+            if (item.depth > 1)
+              console.log('no referrer: ', item.url);
+          }
+          // if we are at top level and only difference between thing we were called with and this
+          // is http://foo.bar.com vs http://foo.bar.com/ then kill the redundant one
+          // If we don't do this then the seed gets left behind with no fetch so we keep re-crawling it
+          // This is soft if there is a hanging referrer ref
+          if (item.depth === 1 && item.url !== url)
+            db.none(SOFTDELETE, [url])
+            .catch(() => {});
+
+          console.log('inserted: ', item.url)
+        } catch (e) {
+          // We mostly aren't interested
+          console.log('insert failed: ', item.url, e)
+        } finally {
+          finalise();
+        }
+      });
+      crawler.on("complete", resolve);
+
+      crawler.start();
     });
-    crawler.on("complete", resolve);
+  } else {
+    crawler.queueURL(url);
+  }
 
-    crawler.start();
-  });
+  return (this.promise);
 
 }
 
